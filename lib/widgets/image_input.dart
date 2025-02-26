44e8ecd4185d59d6405f4d965345cd2174767c42
Akkaranant Khositchotanan <n.akkaranant@gmail.com>
2025-02-26 15:21:31 +0700

    Finished Can Take Cam Now

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,19 +1,63 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-@override
-Widget build(BuildContext context) {
-  return Container(
-    decoration: BoxDecoration(
-      border: Border.all(width: 1, color: Colors.white),
-    ),
-    height: 250,
-    width: double.infinity,
-    alignment: Alignment.center,
-    child: TextButton.icon(
-      onPressed: takePicture,
-      label: Text('Take Picture'),
-      icon: Icon(Icons.camera),
-    ),
-  );
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ImageInputState();
+  }
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File? _selectedImage;
+
+  takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage = await imagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    if (pickedImage == null) {
+      //User not take picture
+      return;
+    }
+
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: Colors.white),
+      ),
+      height: 250,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child:
+          _selectedImage != null
+              ? GestureDetector(
+                onTap: takePicture, // Allow retaking an image by tapping
+                child: Image.file(
+                  _selectedImage!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: double.infinity,
+                ),
+              )
+              : TextButton.icon(
+                icon: const Icon(Icons.camera),
+                label: const Text('Take Picture'),
+                onPressed: takePicture,
+              ),
+    );
+  }
 }
